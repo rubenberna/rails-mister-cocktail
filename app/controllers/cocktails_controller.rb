57afdaebca16d8d452c000f704aca6934affb0b4
ruben bernardes <rubenmbernardes@gmail.com>
2018-03-15 18:14:17 +0100

    added dose_cocktail relation

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,25 +1,23 @@
 class CocktailsController < ApplicationController
-  before_action :set_cocktail, only: [:show]
 
   def index
     @cocktails = Cocktail.all
   end
 
   def show
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
   end
 
   def new
     @cocktail = Cocktail.new
+
   end
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
     @cocktail.save
     redirect_to cocktail_path(@cocktail)
-  end
-
-  def set_cocktail
-    @cocktail = Cocktail.find(params[:id])
   end
 
   def cocktail_params
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,10 +1,17 @@
 class DosesController < ApplicationController
-  def new
-  end
 
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    @dose.save
+    @redirect_to cocktail_path(@cocktail)
   end
 
   def destroy
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
