3ef6eca4c76c0e461972dd0a366ed2b4cb891f1e
ruben bernardes <rubenmbernardes@gmail.com>
2018-03-15 15:00:09 +0100

    controllers and routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktail do
+    resources :dose, only: [:new, :create]
+  end
+  resources :dose, only: [:destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Deleting seeds..."
+Ingredient.destroy_all
+
+puts "Creating seeds..."
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "whiskey")
+Ingredient.create(name: "milk")
+Ingredient.create(name: "pepper")
+Ingredient.create(name: "tomatoe juice")
+Ingredient.create(name: "tabasco")
+Ingredient.create(name: "rum")
+Ingredient.create(name: "orange juice")
+Ingredient.create(name: "tequilla")
